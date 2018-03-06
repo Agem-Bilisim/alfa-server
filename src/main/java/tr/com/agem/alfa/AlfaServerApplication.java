7d3a85d0f39998994e6d5d2eeb0fa550519a18a7
emrekgn <emrekaganakkaya@gmail.com>
2018-03-06 15:46:50 +0300

    Initial commit

---
+++ src/main/java/tr/com/agem/alfa/AlfaServerApplication.java
@@ -0,0 +1,12 @@
+package tr.com.agem.alfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/tr/com/agem/alfa/AlfaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.com.agem.alfa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlfaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
