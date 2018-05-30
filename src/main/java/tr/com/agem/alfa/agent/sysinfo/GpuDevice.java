6a1b48f75565f135042e6323014e51e73c13115e
Emre Akkaya <emrekaganakkaya@gmail.com>
2018-05-30 11:10:26 +0300

    gpu driver version info added

--- src/main/java/tr/com/agem/alfa/agent/sysinfo/GpuDevice.java
+++ src/main/java/tr/com/agem/alfa/agent/sysinfo/GpuDevice.java
@@ -3,6 +3,8 @@ package tr.com.agem.alfa.agent.sysinfo;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GpuDevice implements Serializable {
 
 	private final static long serialVersionUID = 3642930991760174615L;
@@ -10,6 +12,10 @@ public class GpuDevice implements Serializable {
 	private String subsystem;
 	private String kernel;
 	private String memory;
+	@JsonProperty("driver_date")
+	private String driverDate;
+	@JsonProperty("driver_version")
+	private String driverVersion;
 
 	public String getSubsystem() {
 		return subsystem;
@@ -33,6 +39,22 @@ public class GpuDevice implements Serializable {
 
 	public void setMemory(String memory) {
 		this.memory = memory;
+	}
+
+	public String getDriverDate() {
+		return driverDate;
+	}
+
+	public void setDriverDate(String driverDate) {
+		this.driverDate = driverDate;
+	}
+
+	public String getDriverVersion() {
+		return driverVersion;
+	}
+
+	public void setDriverVersion(String driverVersion) {
+		this.driverVersion = driverVersion;
 	}
 
 }
